e9bbadb96c8e9dc5f117be1b1ab73b91c70ee5a3
teja00 <tejanagubandi00@gmail.com>
2024-07-10 23:20:21 -0500

    commit small changes

--- src/App.js
+++ src/App.js
@@ -34,8 +34,8 @@ export default function App() {
       <ambientLight intensity={Math.PI / 2} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
       <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
+      <Box position={[-1.3, 0, 0]} />
+      <Box position={[1.3, 0, 0]} />
       <OrbitControls />
     </Canvas>
   )
